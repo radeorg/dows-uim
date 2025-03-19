443f25c543acee48419de4f9e84885b23e9bcaa3
lait <lait.zhang@gmail.com>
2025-03-19 17:44:54 +0800

    fix

---
+++ uim-boot/src/main/java/org/dows/uim/admin/UimAdminAutoConfig.java
@@ -0,0 +1,20 @@
+package org.dows.uim.admin;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @description: </br>
+ * @author: lait.zhang@gmail.com
+ * @date: 3/20/2024 10:41 AM
+ * @history: </br>
+ * <author>      <time>      <version>    <desc>
+ * 修改人姓名      修改时间        版本号       描述
+ */
+@MapperScan("org.dows.uim.mapper")
+@Configuration
+@ComponentScan(basePackages = {"org.dows.uim"})
+public class UimAdminAutoConfig {
+}
+
---
+++ uim-boot/src/main/java/org/dows/uim/tenant/UimTenantAutoConfig.java
@@ -0,0 +1,21 @@
+package org.dows.uim.tenant;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @description: </br>
+ * @author: lait.zhang@gmail.com
+ * @date: 3/20/2024 10:41 AM
+ * @history: </br>
+ * <author>      <time>      <version>    <desc>
+ * 修改人姓名      修改时间        版本号       描述
+ */
+@MapperScan("org.dows.uim.mapper")
+@Configuration
+@ComponentScan(basePackages = {"org.dows.uim.mapper", "org.dows.uim.service",
+        "org.dows.uim.config", "org.dows.uim.biz", "org.dows.uim.handler", "org.dows.uim.tenant"})
+public class UimTenantAutoConfig {
+}
+
