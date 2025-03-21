365a12af7f115261f80b7846748b35ae610053e7
Libiyou <biyou.li@hosjoy.com>
2025-03-22 01:45:59 +0800

    AAC接口

--- uim-api/src/main/java/org/dows/uim/api/response/AccountIdentifierResponse.java
+++ uim-api/src/main/java/org/dows/uim/api/response/AccountIdentifierResponse.java
@@ -5,6 +5,7 @@ import lombok.Data;
 @Data
 public class AccountIdentifierResponse {
     private Long accountIdentifierId;
+    private Long accountInstanceId;
     private String identifier;
     private Integer type;
     private String appId;
