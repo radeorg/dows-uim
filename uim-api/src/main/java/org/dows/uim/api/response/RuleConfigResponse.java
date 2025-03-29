d28573638b59705dfb0ca1fbd8bf0ecd62868142
lait <lait.zhang@gmail.com>
2025-03-30 04:40:07 +0800

    fix

--- uim-api/src/main/java/org/dows/uim/api/response/RuleConfigResponse.java
+++ uim-api/src/main/java/org/dows/uim/api/response/RuleConfigResponse.java
@@ -1,10 +1,11 @@
 package org.dows.uim.api.response;
 
-import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
+@Schema(description = "规则配置")
 @Data
 public class RuleConfigResponse {
-    @Parameter(description = "岗位规则ID")
+    @Schema(description = "岗位规则ID")
     private Long orgRuleId;
 }
