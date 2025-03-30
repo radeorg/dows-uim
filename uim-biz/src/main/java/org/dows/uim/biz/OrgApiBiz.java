d4ba2cc67b15a8e77ba20899307c1cfc6d812e36
Libiyou <biyou.li@hosjoy.com>
2025-03-30 13:07:47 +0800

    OrgApi接口

--- uim-biz/src/main/java/org/dows/uim/biz/OrgApiBiz.java
+++ uim-biz/src/main/java/org/dows/uim/biz/OrgApiBiz.java
@@ -29,7 +29,7 @@ public class OrgApiBiz implements OrgApi {
     @Override
     public JobIndicatorResponse getOrgIndicatorByJobName(String jobName) {
         JobIndicatorResponse response = new JobIndicatorResponse();
-
+        jobName = "##";
         List<OrgJdEntity> orgJdEntities = QueryChain.of(OrgJdEntity.class)
                 .like(OrgJdEntity::getDescription, jobName, Objects.nonNull(jobName)).list();
         if(Objects.isNull(orgJdEntities) || orgJdEntities.size() == 0){
