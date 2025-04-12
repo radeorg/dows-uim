c0686550da63c82c7e216c16d1526f4c14da24f1
lait <lait.zhang@gmail.com>
2025-04-13 02:30:40 +0800

    自动提交: 合并前的更改

--- uim-app/src/test/java/org/dows/uim/one/GitBatchProcessor.java
+++ uim-app/src/test/java/org/dows/uim/one/GitBatchProcessor.java
@@ -30,8 +30,6 @@ public class GitBatchProcessor {
     private static final int DEFAULT_DELAY_SECONDS = 300;
     
     // 配置您的 GitHub 凭据或 SSH 密钥信息
-    private static final String GITHUB_USERNAME = "lait.zhang@gmail.com";
-    private static final String GITHUB_PASSWORD = "githubz123!";
     private static final String SSH_PRIVATE_KEY_PATH = System.getProperty("user.home") + "/.ssh/id_ecdsa";
     private static final String SSH_PUBLIC_KEY_PATH = System.getProperty("user.home") + "/.ssh/id_ecdsa.pub";
     private static final String SSH_PASSPHRASE = null; // 如果没有密码短语，设为 null 或空字符串
