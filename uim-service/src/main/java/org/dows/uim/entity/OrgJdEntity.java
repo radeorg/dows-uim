bfe2cd8f2deb70748144b99b601f05916954f321
lait <lait.zhang@gmail.com>
2025-04-14 21:36:00 +0800

    feat: 添加人事账号实例ID字段

--- uim-service/src/main/java/org/dows/uim/entity/OrgJdEntity.java
+++ uim-service/src/main/java/org/dows/uim/entity/OrgJdEntity.java
@@ -58,6 +58,10 @@ public class OrgJdEntity extends BaseEntity<OrgJdEntity> {
     @Column(value = "org_rule_id")
     private Long orgRuleId;
 
+    @Schema(description = "人事账号实例ID")
+    @Column(value = "hr_account_instance_id")
+    private Long hrAccountInstanceId;
+
     /**
      * 操作者ID
      */
