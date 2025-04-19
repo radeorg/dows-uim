df18ff035bf4d5d3b6092b62066e4cc68c95eaad
lait <lait.zhang@gmail.com>
2025-04-19 23:54:31 +0800

    feat(uim): 添加 MyBatisFlex 配置类
    - 新增 MyBatisFlexConfig 类，配置 MyBatisFlex 全局设置
    - 添加 SQL 日志打印功能
    - 注册字段填充监听器，用于自动填充 BaseEntity 的子类

---
+++ uim-app/src/main/java/org/dows/uim/MyBatisFlexConfig.java
@@ -0,0 +1,40 @@
+package org.dows.uim;
+
+import com.mybatisflex.core.FlexGlobalConfig;
+import com.mybatisflex.core.audit.AuditManager;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.dows.rade.crud.BaseEntity;
+import org.dows.rade.crud.FieldFillListener;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Slf4j
+@Configuration
+public class MyBatisFlexConfig {
+
+//    private final AppIdContext aacContext;
+
+    @Value("${rade.log.sql.printSql:false}")
+    private boolean printSql;
+
+    @PostConstruct
+    public void init() {
+        if (printSql) {
+            // 开启审计功能
+            AuditManager.setAuditEnable(true);
+            // 设置 SQL 审计收集器
+            AuditManager.setMessageCollector(auditMessage ->
+                    log.info("{},{}ms", auditMessage.getFullSql(), auditMessage.getElapsedTime())
+            );
+        }
+        // 获取全局配置
+        FlexGlobalConfig defaultConfig = FlexGlobalConfig.getDefaultConfig();
+        // 为所有 BaseEntity 的子类添加监听器
+        FieldFillListener fieldFillListener = new FieldFillListener();
+        defaultConfig.registerInsertListener(fieldFillListener, BaseEntity.class);
+        defaultConfig.registerUpdateListener(fieldFillListener, BaseEntity.class);
+    }
+}
