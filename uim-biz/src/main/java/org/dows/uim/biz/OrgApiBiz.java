72c6832aba883f3d328ee9a8cd69650a28f6e70e
Libiyou <biyou.li@hosjoy.com>
2025-04-21 18:01:49 +0800

    注册企业，判断手机号是否重复

--- uim-biz/src/main/java/org/dows/uim/biz/OrgApiBiz.java
+++ uim-biz/src/main/java/org/dows/uim/biz/OrgApiBiz.java
@@ -175,6 +175,15 @@ public class OrgApiBiz {
             if (Objects.nonNull(orgEmailEntities) && orgEmailEntities.size() > 0) {
                 throw new UimException(item.getOrgName() + "， 【" + item.getEmail() + "】邮箱已存在，无法保存");
             }
+
+            if(Objects.nonNull(item.getTelephone())) {
+                List<AccountIdentifierEntity> accountIdentifierEntities = QueryChain.of(AccountIdentifierEntity.class)
+                        .eq(AccountIdentifierEntity::getIdentifier, item.getTelephone(), Objects.nonNull(item.getTelephone()))
+                        .eq(AccountIdentifierEntity::getIdentifierType, IdentifierType.PHONE.getType()).list();
+                if (Objects.nonNull(accountIdentifierEntities) && accountIdentifierEntities.size() > 0) {
+                    throw new UimException(item.getOrgName() + "， 【" + item.getTelephone() + "】手机号已存在，无法保存");
+                }
+            }
         }
 
         // 批量保存组织树
