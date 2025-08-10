ecb70a24e49de0dd1eaa349aa5f2412081b9d49b
Tangsimin51 <495288201@qq.com>
2025-08-10 11:14:06 +0800

    简历解析、岗位匹配

---
+++ uim-api/src/main/java/org/dows/uim/response/OrgJdResponse.java
@@ -0,0 +1,30 @@
+package org.dows.uim.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+/**
+ * 岗位JD表 实体类。
+ */
+@Data
+@Schema(name = "岗位JD表")
+public class OrgJdResponse {
+
+    @Schema(description = "岗位描述ID")
+    private Long orgJdId;
+
+    @Schema(description = "组织树rootID")
+    private Long orgRootId;
+
+    @Schema(description = "组织树ID")
+    private Long orgTreeId;
+
+    @Schema(description = "岗位编号")
+    private String jdNo;
+
+    @Schema(description = "岗位名称+岗位编号")
+    private String jdName;
+
+    @Schema(description = "岗位标签内容")
+    private String tagContent;
+}
